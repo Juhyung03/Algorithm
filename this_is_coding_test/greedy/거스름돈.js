f9e3128c38a85d137b70ae95788e7adc6c166052
juhyung <njh1019@gmail.com>
2026-05-15 17:24:21 +0900

    solve: 그리디 알고리즘 거스름돈 문제 풀이

---
+++ this_is_coding_test/greedy/거스름돈.js
@@ -0,0 +1,13 @@
+let n = 1260;
+let count = 0;
+
+const coinTypes = [500, 100, 50, 10];
+
+for (const coin of coinTypes){
+  let num = Math.floor(n / coin);
+  count += num;
+  n = n - (num * coin);
+  console.log(num)
+}
+
+console.log('최소 동전의 개수: ', count)

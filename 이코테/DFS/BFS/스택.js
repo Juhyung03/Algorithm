5f7f7d88b9e97c51111e2de03b324bf8d0ca7f08
Juhyung03 <njhzi1019@gmail.com>
2026-07-07 16:26:44 +0900

    solve: DFS/BFS 알고리즘 스택 문제 풀이

---
+++ 이코테/DFS/BFS/스택.js
@@ -0,0 +1,15 @@
+//🔁
+// stack은 push , pop 사용
+let stack = [];
+
+stack.push(5);
+stack.push(2);
+stack.push(3);
+stack.push(7);
+stack.pop();
+stack.push(1);
+stack.push(4);
+stack.pop();
+
+console.log(stack);
+console.log([...stack].reverse());

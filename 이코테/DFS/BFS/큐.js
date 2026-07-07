1f8d663ae847dd41b0c95e3dce1e7843905e997a
Juhyung03 <njhzi1019@gmail.com>
2026-07-07 16:27:11 +0900

    solve: DFS/BFS 알고리즘 큐 문제 풀이

---
+++ 이코테/DFS/BFS/큐.js
@@ -0,0 +1,16 @@
+//🔁
+// queue은 push , shift 사용
+
+let queue = [];
+
+queue.push(5);
+queue.push(2);
+queue.push(3);
+queue.push(7);
+queue.shift();
+queue.push(1);
+queue.push(4);
+queue.shift();
+
+console.log(queue);
+console.log([...queue].reverse());

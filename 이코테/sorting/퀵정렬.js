d46191580195de15eca0bc57799beb1ab40c548a
Juhyung03 <njhzi1019@gmail.com>
2026-07-16 18:35:43 +0900

    fix: 정렬알고리즘 퀵정렬 오류 수정

--- 이코테/sorting/퀵정렬.js
+++ 이코테/sorting/퀵정렬.js
@@ -1,33 +1,30 @@
 //🔁
-function quickSort(array, start, end){
-  if (start >= end){
-    return;
+function quickSort(arr, start, end){
+  if (start >= end) return;
 
-    const pivot = arr[start];
-    let left = start + 1;
-    let right = end;
+  let pivot = arr[start];
+  let left = start + 1;
+  let right = end;
 
-    while (left <= right){
-      while (left <= end && arr[left] <= pivot){
-        left++;
-      }
-      while (right > start && arr[right] >= pivot){
-        right--;
-      }
-
-      if (left > right){
-        [arr[start], arr[right]] = [arr[right], arr[start]];
-      }else {
-        [arr[left], arr[right]] = [arr[right], arr[left]];
-      }
+  while (left <= right){
+    while (left <= end && arr[left] <= pivot){
+      left++;
     }
-
-    quickSort(arr, start, right-1);
-    quickSort(arr, right+1, end);
+    while (right > start && arr[right] >= pivot){
+      right--;
+    }
+    if (left > right){
+      [arr[start], arr[right]] = [arr[right], arr[start]];
+    }else {
+      [arr[left], arr[right]] = [arr[right], arr[left]];
+    }
   }
 
-  const arr = [5,7,9,0,3,1,6,2,4,8];
-
-  quickSort(arr, 0, arr.length - 1);
-  console.log(arr);
+  quickSort(arr, start, right-1);
+  quickSort(arr, right+1, end);
 }
+
+const arr = [5, 7, 9, 0, 3, 1, 6, 2, 4, 8];
+
+quickSort(arr, 0, arr.length - 1);
+console.log(arr);

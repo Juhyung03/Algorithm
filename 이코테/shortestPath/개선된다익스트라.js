a378612237b703d5c7c5927b012a50391d1b9d0b
Juhyung03 <njhzi1019@gmail.com>
2026-08-04 19:28:22 +0900

    fix: 주석 추가 및 실행 코드 추가

--- 이코테/shortestPath/개선된다익스트라.js
+++ 이코테/shortestPath/개선된다익스트라.js
@@ -1,4 +1,21 @@
 //🔁🔁
+//입력받기
+const fs = require("fs");
+const input = fs.readFileSync(0,"utf8").trim().split("\n");
+
+//n: 노드 개수, m: 간선 개수
+const [n, m] = input[0].split(" ").map(Number);
+//시작 노드 번호
+const start = Number(input[1]);
+//그래프 초기화
+const graph = Array.from({length: n+1}, () => []); 
+//간선 정보를 grpah에 추가
+for (let i = 2; i < 2 + m; i++){
+  const [from, to, cost] = input[i].split(" ").map(Number);
+  graph[from].push([to, cost]);
+}
+
+//무한 상수 설정
 const INF = 1e9;
 
 //최소 힙 구현
@@ -78,29 +95,38 @@ class PriorityQueue {
 
 //다익스트라
 function dijkstra(start, graph, n){
-  const distance = Array(n+1).fill(INF);
-    const pq = new PriorityQueue();
+  //거리 배열 초기화
+  const distance = Array(n + 1).fill(INF);
+  //우선순위 큐 추가
+  const pq = new PriorityQueue();
 
-    distance[start] = 0;
-    pq.push([0, start]); //[거리, 노드]
+  distance[start] = 0;
+  pq.push([0, start]); //[거리, 노드]
 
-    while (!pq.isEmpty()){
-      const [dist, now] = pq.pop();
+  while (!pq.isEmpty()){
+    const [dist, now] = pq.pop();
 
-      //이미 더 짧은 경로를 알고 있으면 무시
-      if (dist > distance[now]){
-        continue;
-      }
-
-      for (const [next, cost] of graph[now]){
-        const newCost = dist + cost;
-
-        if (newCost < distance[next]){
-          distance[next] = newCost;
-          pq.push([newCost, next]);
-        }
-      }
+    //이미 더 짧은 경로를 알고 있으면 무시
+    if (dist > distance[now]){
+      continue;
     }
 
-    return distance;
+    for (const [next, cost] of graph[now]){
+      const newCost = dist + cost;
+
+      if (newCost < distance[next]){
+        distance[next] = newCost;
+        pq.push([newCost, next]);
+      }
+    }
+  }
+  return distance;
 }
+
+//실행
+const result = dijkstra(start, graph, n);
+//출력
+for (let i =1; i <= n; i++){
+  console.log(result[i] === INF ? "INF" : result[i]);
+}
+
